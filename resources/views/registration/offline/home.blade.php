9ea29dbcf5f4ba3a61ec66a57dba29aea16686a1
taufiqridha <taufiq.ridha@gmail.com>
2022-02-16 16:07:52 +0700

    - resize font kiosk print ticket

--- resources/views/registration/offline/home.blade.php
+++ resources/views/registration/offline/home.blade.php
@@ -4,11 +4,11 @@
 
     <div class="to-print">
 
-            <p class="text-center" style="margin:0; font-size: 20pt;line-height: 5px;margin-bottom: 30px;margin-top:10px">PASKER.ID</p>
+            <p class="text-center" style="margin:0; font-size: 15pt;line-height: 5px;margin-bottom: 15px;margin-top:10px">PASKER.ID</p>
             <p id="print-date" class="text-center" style="margin:0;font-size: 10pt;line-height: 5px;margin-bottom: 30px">Senin, 20 Desember 2022 - 10:30:22</p>
             <p class="text-center" style="margin:0;font-size: 16pt;line-height: 5px;margin-bottom: 40px">Nomor Antrian</p>
             <p id="print-token" class="text-center" style="margin:0;font-size: 30pt;line-height: 5px;margin-bottom: 40px">A030</p>
-            <p id="print-layanan" class="text-center" style="margin:0;font-size: 20pt;line-height: 5px"><strong>Pelayanan Komuk</strong></p>
+            <p id="print-layanan" class="text-center" style="margin:0;font-size: 15pt;line-height: 5px"><strong>Pelayanan Komuk</strong></p>
 
     </div>
 
